1b648ef9fa4079cecb915252be2b697452dc5806
Tjeerd Ritsma <tjeerd@mollie.com>
2017-09-23 17:04:18 +0200

    improve test error message

--- example_test.go
+++ example_test.go
@@ -11,11 +11,11 @@ func TestNew(t *testing.T) {
 	module := New()
 
 	if module.Name() != "TestModule" {
-		t.Error("Expected version 0.0.1, got ", module.Name())
+		t.Error("Expected module name TestModule, got ", module.Name())
 	}
 
 	if module.Version() != "0.0.1" {
-		t.Error("Expected version 0.0.1, got ", module.Version())
+		t.Error("Expected module version 0.0.1, got ", module.Version())
 	}
 }
 

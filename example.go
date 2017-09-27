37da93ac6beaf1dfdb20432f748e8240e6d80678
Tjeerd Ritsma <tjeerd@mollie.com>
2017-09-28 00:07:10 +0200

    Update to work with ModularSlackBot 0.1.0

--- example.go
+++ example.go
@@ -3,7 +3,7 @@ package example
 import (
 	"fmt"
 
-	"github.com/thewolfnl/ModularSlackBot/bot"
+	"github.com/thewolfnl/ModularSlackBot"
 )
 
 type TestModule struct {
@@ -11,20 +11,22 @@ type TestModule struct {
 }
 
 // New function to return a new instance of this bot
-func New() TestModule {
+func New() *TestModule {
 	module := TestModule{bot.NewModule("TestModule", "0.0.1")}
 
 	// Define triggers
 	module.AddTrigger("(?i)(hello|hi|hey).*", module.hello)
 
-	module.AddTrigger("test", func(message bot.Message) {
+	module.AddTrigger("test", func(message *bot.Message) error {
 		fmt.Printf("\nMessageJSON:\n%s\n", message.ToJson())
 		module.Respond("Read you loud and clear..")
+		return nil
 	})
 
-	return module
+	return &module
 }
 
-func (module *TestModule) hello(message bot.Message) {
+func (module *TestModule) hello(message *bot.Message) error {
 	module.Respond("Hey there, to you too")
+	return nil
 }
--- example_test.go
+++ example_test.go
@@ -1,10 +1,9 @@
 package example
 
 import (
-	"encoding/json"
 	"testing"
 
-	"github.com/thewolfnl/ModularSlackBot/bot"
+	"github.com/thewolfnl/ModularSlackBot"
 )
 
 func TestNew(t *testing.T) {
@@ -21,11 +20,7 @@ func TestNew(t *testing.T) {
 
 func ExampleHello() {
 	module := New()
-	message, err := createMessage("Hello")
-	if err != nil {
-		// exit
-	}
-	module.HandleInput(message)
+	module.HandleInput(bot.CreateMessage("Hello"))
 	// Output: Sending to #C2147483705
 	// Response: Hey there, to you too
 	// Message not sent to slack because slack api is not configured
@@ -33,38 +28,11 @@ func ExampleHello() {
 
 func ExampleTest() {
 	module := New()
-	message, err := createMessage("test")
-	if err != nil {
-		// exit
-	}
-	module.HandleInput(message)
+	module.HandleInput(bot.CreateMessage("test"))
 	// Output: MessageJSON:
-	// {"type":"message","channel":"C2147483705","user":"U2147483697","text":"test","ts":"1355517523.000005","pinned_to":null}
+	// {"channel":"C2147483705","user":"U2147483697","text":"test","pinned_to":null}
 	//
 	// Sending to #C2147483705
 	// Response: Read you loud and clear..
 	// Message not sent to slack because slack api is not configured
-}
-
-func createMessage(messageString string) (bot.Message, error) {
-	message := bot.Message{}
-	messageJson := `{
-		"type": "message",
-		"channel": "C2147483705",
-		"user": "U2147483697",
-		"text": "Hello world",
-		"ts": "1355517523.000005"
-		}`
-	if err := json.Unmarshal([]byte(messageJson), &message); err != nil {
-		return bot.Message{}, err
-	}
-	message.Text = messageString
-	return message, nil
-}
-
-func TestCreateMessage(t *testing.T) {
-	_, err := createMessage("Hello")
-	if err != nil {
-		t.Error("Could not create a test message")
-	}
 }
